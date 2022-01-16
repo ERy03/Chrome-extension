35b76cca3a681bc9203f1241ebc93e9638844881
Ryota <ryoenoki2013@gmail.com>
2022-01-17 00:48:18 +0900

    refactoring

--- index.js
+++ index.js
@@ -7,34 +7,33 @@ const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    renderLeads()
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+
+function render(leads) {
+  let listItems = ""
+  for(let i = 0; i < leads.length; i ++) {
+    listItems += `
+    <li>
+      <a href= '${leads[i]}' target='_blank'>
+        ${leads[i]}
+      </a>
+    </li>
+    `
+  }
+  ulEl.innerHTML = listItems
 }
 
 deleteBtn.addEventListener("dblclick", function() {
   localStorage.clear()
   myLeads = []
-  renderLeads()
+  render(myLeads)
 })
 
 inputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value)
   inputEl.value = ""
   localStorage.setItem("myLeads", JSON.stringify(myLeads))
-  renderLeads()
+  render(myLeads)
 })
-
-function renderLeads() {
-  let listItems = ""
-  for(let i = 0; i < myLeads.length; i ++) {
-    listItems += `
-    <li>
-      <a href= '${myLeads[i]}' target='_blank'>
-        ${myLeads[i]}
-      </a>
-    </li>
-    `
-  }
-  ulEl.innerHTML = listItems
-  console.log(listItems);
-}
